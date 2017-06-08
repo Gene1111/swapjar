25c7eb1dc254edf57b71d9af8bb62105003860f6
Administrator <Administrator@LONGYUANJI>
2017-06-08 08:59:55 +0800

    init

---
+++ src/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package com;
+
+public class HelloWorld {
+
+	
+	public static void print() {
+		System.out.println("hello world11");
+		Test1 t = new Test1();
+		t.setT(1000);
+		System.out.println("test1 " + t.getT());
+	}
+	
+}
---
+++ src/com/Test1.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Test1 {
+
+	private int t;
+
+	public int getT() {
+		return t;
+	}
+
+	public void setT(int t) {
+		this.t = t;
+	}
+}
